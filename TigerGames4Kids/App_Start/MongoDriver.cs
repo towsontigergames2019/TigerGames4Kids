5714785ef417d87e1b154c65fa7062389106c6af
Nick Visalli <nvisal1@students.towson.edu>
2019-05-11 13:30:31 -0400

    Create MongoDriver class

---
+++ TigerGames4Kids/App_Start/MongoDriver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using MongoDB.Driver;
+
+namespace TigerGames4Kids.App_Start 
+{   
+    public class MongoDriver 
+    {
+        MongoClient _client;
+        MongoServer _server;
+        public MongoDatabase _database;
+
+        public MongoDriver()
+        {
+            var MongoDataBaseName = ConfigurationManager.AppSettings["MongoDatabaseName"];
+            var MongoUsername = ConfigurationManager.AppSettings["MongoUsername"];
+            var MongoPassword = ConfigurationManager.AppSettings["MongoPassword"];
+            var MongoPort = ConfigurationManager.AppSettings["MongoPort"];
+            var MongoHost = ConfigurationManager.AppSettings["MongoHost"];
+
+            var credential = MongoCredential.CreateMongoCRCredential(MongoDataBaseName, MongoUsername, MongoPassword);
+
+            var settings = new MongoClientSettings
+            {
+                Credentials = new[] { credential },
+                Server = new MongoServerAddress(MongoHost, Convert.ToInt32(MongoPort))
+            };
+            _client = new MongoClient(settings);
+            _server = _client.GetServer();
+            _database = _server.GetDatabase(MongoDataBaseName);
+        }
+    }
+}
+
