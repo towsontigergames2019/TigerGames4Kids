dfb0f6a953afc1f842725d47240ff11866e5efc5
Nick Visalli <nvisal1@students.towson.edu>
2019-05-11 13:46:38 -0400

    decorate game type model with MongoDB attributes

---
+++ TigerGames4Kids/Models/GameType.cs
@@ -0,0 +1,22 @@
+﻿
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TigerGames4Kids.Models
+{
+    public class GameType
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Title")]
+        public string Title { get; set; }
+
+        [BsonElement("URI")]
+        public string URI { get; set; }
+
+        [BsonElement("Genre")]
+        public string Genre { get; set; }
+    }
+}
