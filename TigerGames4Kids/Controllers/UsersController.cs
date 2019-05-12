b2bc81f062b01efba38dd2382f5278eb86f51f85
Nick Visalli <nvisal1@students.towson.edu>
2019-05-11 20:27:33 -0400

    Generate Profile Image URL on registration

--- TigerGames4Kids/Controllers/UsersController.cs
+++ TigerGames4Kids/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using MongoDB.Bson;
 using TigerGames4Kids.App_Start;
 using TigerGames4Kids.Models;
+using System.Text;
 
 namespace TigerGames4Kids.Controllers
 {
@@ -16,6 +17,24 @@ namespace TigerGames4Kids.Controllers
             _dbConnection = new MongoDriver();
         }
 
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
         public ActionResult Register()
         {
             return View();
@@ -27,6 +46,10 @@ namespace TigerGames4Kids.Controllers
         public ActionResult Create(UserType user)
         {
             var collection = _dbConnection._database.GetCollection<UserType>("Users");
+
+            var hash = CreateMD5(user.Email);
+
+            user.ProfileImageURI = "https://www.gravatar.com/avatar/" + hash + "?s=200?r=pg&d=identicon";
 
             collection.InsertOne(user);
      
