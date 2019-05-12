fb497451cfead9b0f0b9faa6bec94a1d465f9f0b
Nick Visalli <nvisal1@students.towson.edu>
2019-05-11 23:02:48 -0400

    Create function for setting session info

--- TigerGames4Kids/Controllers/UsersController.cs
+++ TigerGames4Kids/Controllers/UsersController.cs
@@ -4,6 +4,8 @@ using MongoDB.Bson;
 using TigerGames4Kids.App_Start;
 using TigerGames4Kids.Models;
 using System.Text;
+using MongoDB.Driver;
+using System;
 
 namespace TigerGames4Kids.Controllers
 {
@@ -35,11 +37,28 @@ namespace TigerGames4Kids.Controllers
             }
         }
 
+        public static bool verifyMd5Hash(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = CreateMD5(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public ActionResult Register()
         {
             return View();
         }
-
 
         // POST: Users/Create
         [HttpPost]
@@ -54,8 +73,44 @@ namespace TigerGames4Kids.Controllers
             user.Password = CreateMD5(user.Password);
 
             collection.InsertOne(user);
-     
+
             return RedirectToAction("Home");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Users/Token
+        [HttpPost]
+        public ActionResult Token(UserType user)
+        {
+            var collection = _dbConnection._database.GetCollection<UserType>("Users");
+
+            var filter = new BsonDocument("Username", user.Username);
+
+            var userInfo = collection.FindSync<UserType>(filter).ToList();
+
+            if (userInfo[0] != null && verifyMd5Hash(user.Password, userInfo[0].Password))
+            {
+                Session["Username"] = userInfo[0].Username;
+                Session["Email"] = userInfo[0].Email;
+                Session["Age"] = userInfo[0].Age;
+                Session["ProfileImageURI"] = userInfo[0].ProfileImageURI;
+                return RedirectToAction("Home");
+            }
+            else
+            {
+                var message = "Incorrect Login";
+                return View("Login", message);
+            }
+        }
+
+        public ActionResult View()
+        {
+            var collection = _dbConnection._database.GetCollection<UserType>("Users");
+            return View();
         }
     }
 }
