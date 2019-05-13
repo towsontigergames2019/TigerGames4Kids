bd4accef1b2b6f06f86563e3774415d09693e895
Nick Visalli <nvisal1@students.towson.edu>
2019-05-13 16:41:52 -0400

    Scaffold GamesController

--- TigerGames4Kids/Controllers/GamesController.cs
+++ TigerGames4Kids/Controllers/GamesController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using TigerGames4Kids.App_Start;
 using TigerGames4Kids.Models;
 
 
@@ -10,11 +9,168 @@ namespace TigerGames4Kids.Controllers
 {
     public class GamesController : Controller
     {
-        // GET: Games/Random
-        public ActionResult Random()
+
+        MongoDriver _dbConnection;
+
+        public GamesController()
         {
-            var game = new GameType() { Title = "Super Mario" };
-            return View(game);
+            _dbConnection = new MongoDriver();
+        }
+
+        // GET: Games/AllGames
+        public ActionResult AllGames()
+        {
+            var collection = _dbConnection._database.GetCollection<UserType>("Games");
+
+            var filter = new BsonDocument();
+
+            var games = collection.FindSync<UserType>(filter).ToList();
+            return View(games);
+        }
+
+        // GET: Games/Show/:title
+        public ActionResult Show(string title)
+        {
+
+            var collection = _dbConnection._database.GetCollection<UserType>("Games");
+
+            var filter = new BsonDocument("Title", title);
+
+            var games = collection.FindSync<UserType>(filter).ToList();
+            return View(games[0]);
+        }
+
+        // GET: Games/Add
+        public ActionResult Add()
+        {
+            if (Session["Username"] != null)
+            {
+                if (Session["Role"] == "Admin")
+                { 
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("ViewUser");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // POST: Games/AddGame
+        public ActionResult AddGame(GameType game)
+        {
+            if (Session["Username"] != null)
+            {
+                if (Session["Role"] == "Admin")
+                {
+                    var collection = _dbConnection._database.GetCollection<GameType>("Games");
+                    collection.InsertOne(game);
+                    return RedirectToAction("ViewUser");
+                }
+                else
+                {
+                    return RedirectToAction("ViewUser");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // GET: Games/Edit/:title
+        public ActionResult Edit(string title)
+        {
+            if (Session["Username"] != null)
+            {
+                if (Session["Role"] == "Admin")
+                {
+                    var collection = _dbConnection._database.GetCollection<UserType>("Games");
+                    var filter = new BsonDocument("Title", title);
+                    var games = collection.FindSync<UserType>(filter).ToList();
+                    return View(games[0]);
+                }
+                else
+                {
+                    return RedirectToAction("ViewUser");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // POST: Games/EditGame
+        public ActionResult EditGame(GameType game)
+        {
+            if (Session["Username"] != null)
+            {
+                if (Session["Role"] == "Admin")
+                {
+                    var collection = _dbConnection._database.GetCollection<GameType>("Games");
+                    collection.InsertOne(game);
+                    return RedirectToAction("ViewUser");
+                }
+                else
+                {
+                    return RedirectToAction("ViewUser");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // GET: Games/Delete/:title
+        public ActionResult Delete(string title)
+        {
+            if (Session["Username"] != null)
+            {
+                if (Session["Role"] == "Admin")
+                {
+                    var collection = _dbConnection._database.GetCollection<UserType>("Games");
+                    var filter = new BsonDocument("Title", title);
+                    var games = collection.FindSync<UserType>(filter).ToList();
+                    return View(games[0]);
+                }
+                else
+                {
+                    return RedirectToAction("ViewUser");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // POST: Games/DeleteGame
+        public ActionResult DeleteGame(GameType game)
+        {
+            if (Session["Username"] != null)
+            {
+                if (Session["Role"] == "Admin")
+                {
+                    var collection = _dbConnection._database.GetCollection<GameType>("Games");
+                    var filter = new BsonDocument("Title", game.Title);
+                    collection.DeleteOne(filter);
+                    return RedirectToAction("ViewUser");
+                }
+                else
+                {
+                    return RedirectToAction("ViewUser");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
     }
 }
