44be356b1f2e840033797dcd2d86c5a70b5b760e
Jennifer Jones <jenniferalexisjones@gmail.com>
2019-05-19 14:56:36 -0400

    start stripe controller

---
+++ TigerGames4Kids/Controllers/StripeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TigerGames4Kids.Controllers
+{
+    public class StripeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
